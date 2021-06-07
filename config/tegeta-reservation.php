37f247ad965ca4689b2af4668001a1f7cd7c0a9d
CSenshi <saba.pochkhua@gmail.com>
2021-06-08 01:48:27 +0400

    add basic resrvation service with tests

--- config/tegeta-reservation.php
+++ config/tegeta-reservation.php
@@ -1,5 +1,17 @@
 <?php
-// config for Reddot/ClassName
+
 return [
 
+    'url' => env('TEGETA_RESERVATION_URL', 'http://10.11.11.42'),
+
+    'code' => env('TEGETA_RESERVATION_CODE', '001'),
+
+    'environments' => [
+        'testing' => [
+            'url' => 'http://10.11.11.42',
+        ],
+        'production' => [
+            'url' => '',
+        ],
+    ],
 ];
---
+++ tests/TegetaReservationTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Reddot\TegetaReservation\Tests;
+
+use Carbon\Carbon;
+use Reddot\TegetaReservation\TegetaReservation;
+
+class TegetaReservationTest extends TestCase
+{
+    /** @test */
+    public function test_config()
+    {
+        $config = config('tegeta-reservation');
+        $this->assertIsArray($config);
+
+        $code = config('tegeta-reservation.code');
+        $this->assertIsString($code);
+
+        $url = config('tegeta-reservation.url');
+        $this->assertIsString($url);
+    }
+
+    /** @test */
+    public function test_api_1_reservation_information()
+    {
+        $tegetaReservation = new TegetaReservation;
+        $reservationInformation = $tegetaReservation->reservationInformation();
+
+        $this->assertIsArray($reservationInformation);
+    }
+
+    /** @test */
+    public function test_api_2_reservation_information()
+    {
+        $tegetaReservation = new TegetaReservation;
+
+        $reservationInformation = $tegetaReservation->reservationInformationFiltered('ცენტრალური', 'სავალი ნაწილი', (Carbon::tomorrow()->format('Y-m-d')));
+
+        $this->assertIsArray($reservationInformation);
+        $this->assertArrayHasKey('ცენტრალური', $reservationInformation);
+        $this->assertArrayHasKey('სავალი ნაწილი', $reservationInformation['ცენტრალური']);
+
+        $this->assertArrayHasKey('reservation_times', $reservationInformation['ცენტრალური']['სავალი ნაწილი']);
+        $this->assertArrayHasKey('not_available_datetimes', $reservationInformation['ცენტრალური']['სავალი ნაწილი']);
+
+        $this->assertIsArray($reservationInformation['ცენტრალური']['სავალი ნაწილი']['reservation_times']);
+        $this->assertIsArray($reservationInformation['ცენტრალური']['სავალი ნაწილი']['not_available_datetimes']);
+    }
+
+    /** @test */
+    public function test_api_3_reservation_information_month()
+    {
+        $tegetaReservation = new TegetaReservation;
+
+        $reservationInformationMonth = $tegetaReservation->reservationInformationMonth('ცენტრალური', 'სავალი ნაწილი', Carbon::tomorrow()->format('Y'), Carbon::tomorrow()->format('m'));
+
+        $this->assertIsArray($reservationInformationMonth);
+        $this->assertArrayHasKey('ცენტრალური', $reservationInformationMonth);
+        $this->assertArrayHasKey('available_datetimes', $reservationInformationMonth['ცენტრალური']);
+        $this->assertArrayHasKey('not_available_datetimes', $reservationInformationMonth['ცენტრალური']);
+
+        $this->assertIsArray( $reservationInformationMonth['ცენტრალური']['available_datetimes']);
+        $this->assertIsArray( $reservationInformationMonth['ცენტრალური']['not_available_datetimes']);
+    }
+}
