d9b98b4a708866ec6e585be62d408b67a425e08b
CSenshi <saba.pochkhua@gmail.com>
2021-06-09 03:53:34 +0400

    move controller logic to service

--- src/Http/Controllers/ReservationApiController.php
+++ src/Http/Controllers/ReservationApiController.php
@@ -6,6 +6,7 @@ use DateTime;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Reddot\TegetaReservation\Facades\ReservationService;
 use Reddot\TegetaReservation\Facades\ReservationServiceApi;
 use Reddot\TegetaReservation\Http\Middleware\ForceJsonResponse;
 use Reddot\TegetaReservation\Http\Requests\DatesNMonthRequest;
@@ -21,7 +22,7 @@ class ReservationApiController extends Controller
         $this->middleware(ForceJsonResponse::class);
     }
 
-    public function branches(Request $request): JsonResponse
+    public function branches(): JsonResponse
     {
         $reservationInformation = ReservationServiceApi::reservationInformation();
 
@@ -32,87 +33,34 @@ class ReservationApiController extends Controller
 
     public function services(ServicesRequest $request): JsonResponse
     {
-        $reservationInformation = ReservationServiceApi::reservationInformation();
-
-        if (! array_key_exists($request->branch, $reservationInformation)) {
-            abort(404);
-        }
-
         return response()->json([
-            'services' => $reservationInformation[$request->branch],
+            'services' => ReservationService::getServicesFromRequest($request),
         ]);
     }
 
     public function dates(DatesRequest $request): JsonResponse
     {
-        $reservationInformationMonth = ReservationServiceApi::reservationInformationMonth($request->branch, $request->service_type, $request->year, $request->month);
-
-        if (! array_key_exists($request->branch, $reservationInformationMonth)) {
-            abort(404);
-        }
+        $datetitmes = ReservationService::getAvailableDatetimesFromRequest($request);
 
         return response()->json([
-            'available' => array_map(fn ($d) => date_format(date_create($d), "m/d/Y"), $reservationInformationMonth[$request->branch]['available_datetimes']),
-            'not_available' => array_map(fn ($d) => date_format(date_create($d), "m/d/Y"), $reservationInformationMonth[$request->branch]['not_available_datetimes']),
+            'available' => $datetitmes['available'],
+            'not_available' => $datetitmes['not_available'],
         ]);
     }
 
     public function datesNMonth(DatesNMonthRequest $request): JsonResponse
     {
-        $date = DateTime::createFromFormat('m-Y', $request->month . '-' . $request->year);
-        $resultAvailable = [];
-        $resultNotAvailable = [];
-
-        for ($i = 0; $i < $request->n; $i++) {
-            $year = $date->format('Y');
-            $month = $date->format('m');
-            $reservationInformationMonth = ReservationServiceApi::reservationInformationMonth($request->branch, $request->service_type, $year, $month);
-
-            if (! array_key_exists($request->branch, $reservationInformationMonth)) {
-                abort(404);
-            }
-
-            array_push($resultAvailable, array_map(fn ($d) => date_format(date_create($d), "m/d/Y"), $reservationInformationMonth[$request->branch]['available_datetimes']));
-            array_push($resultNotAvailable, array_map(fn ($d) => date_format(date_create($d), "m/d/Y"), $reservationInformationMonth[$request->branch]['not_available_datetimes']));
-
-            date_add($date, date_interval_create_from_date_string('1 month'));
-        }
+        $datetitmes = ReservationService::getAvailableDatetimesForMultiMonthFromRequest($request);
 
         return response()->json([
-            'available' => array_values(array_unique(Arr::flatten($resultAvailable))),
-            'not_available' => array_values(array_unique(Arr::flatten($resultNotAvailable))),
+            'available' => $datetitmes['available'],
+            'not_available' => $datetitmes['not_available'],
         ]);
     }
 
     public function times(TimesRequest $request): JsonResponse
     {
-        $reservationInformationMonth = ReservationServiceApi::reservationInformationFiltered($request->branch, $request->service_type, $request->date);
-
-        if (
-            ! array_key_exists($request->branch, $reservationInformationMonth) ||
-            ! array_key_exists($request->service_type, $reservationInformationMonth[$request->branch])
-        ) {
-            abort(404);
-        }
-
-        $times = $reservationInformationMonth[$request->branch][$request->service_type];
-
-        $times['available'] = [];
-        foreach ($times['reservation_times'] as $reservationTime) {
-            $available = true;
-
-            foreach ($times['not_available_datetimes'] as $notAvailableTime) {
-                if (str_contains($notAvailableTime, $reservationTime)) {
-                    $available = false;
-
-                    break;
-                }
-            }
-
-            if ($available) {
-                array_push($times['available'], $reservationTime);
-            }
-        }
+        $times = ReservationService::getAvailableTimesFromRequest($request);
 
         return response()->json([
             'times' => $times,
@@ -121,18 +69,8 @@ class ReservationApiController extends Controller
 
     public function reserve(ReserveRequest $request): JsonResponse
     {
-        $reserveResult = ReservationServiceApi::reserve(
-            $request->plate_number,
-            $request->car_type,
-            $request->user_type,
-            $request->IDNumber,
-            $request->company_id,
-            $request->branch,
-            $request->service,
-            $request->date,
-            $request->time,
-            $request->phone
-        );
+
+        $reserveResult = ReservationService::storeReservationFromRequest($request);
 
         return response()->json($reserveResult);
     }
--- src/ReservationService.php
+++ src/ReservationService.php
@@ -2,6 +2,125 @@
 
 namespace Reddot\TegetaReservation;
 
+use DateTime;
+use Illuminate\Support\Arr;
+use Reddot\TegetaReservation\Facades\ReservationServiceApi;
+use Reddot\TegetaReservation\Http\Requests\DatesNMonthRequest;
+use Reddot\TegetaReservation\Http\Requests\DatesRequest;
+use Reddot\TegetaReservation\Http\Requests\ReserveRequest;
+use Reddot\TegetaReservation\Http\Requests\ServicesRequest;
+use Reddot\TegetaReservation\Http\Requests\TimesRequest;
+
 class ReservationService
 {
+    public function getServices(string $branch)
+    {
+        $reservationInformation = ReservationServiceApi::reservationInformation();
+
+        if (!array_key_exists($branch, $reservationInformation)) {
+            abort(404);
+        }
+
+        return $reservationInformation[$branch];
+    }
+
+    public function getAvailableDatetimes($branch, $service_type, $year, $month)
+    {
+        $reservationInformationMonth = ReservationServiceApi::reservationInformationMonth($branch, $service_type, $year, $month);
+
+        if (!array_key_exists($branch, $reservationInformationMonth)) {
+            abort(404);
+        }
+
+        return [
+            'available' => array_map(fn ($d) => date_format(date_create($d), "m/d/Y"), $reservationInformationMonth[$branch]['available_datetimes']),
+            'not_available' => array_map(fn ($d) => date_format(date_create($d), "m/d/Y"), $reservationInformationMonth[$branch]['not_available_datetimes']),
+        ];
+    }
+
+    public function getAvailableDatetimesForMultiMonth($branch, $service_type, $year, $month, $n)
+    {
+        $date = DateTime::createFromFormat('m-Y', $month . '-' . $year);
+        $resultAvailable = [];
+        $resultNotAvailable = [];
+
+        for ($i = 0; $i < $n; $i++) {
+            $year = $date->format('Y');
+            $month = $date->format('m');
+            $datetitmes = $this->getAvailableDatetimes($branch, $service_type, $year, $month);
+
+            array_push($resultAvailable, $datetitmes['available']);
+            array_push($resultNotAvailable, $datetitmes['not_available']);
+
+            date_add($date, date_interval_create_from_date_string('1 month'));
+        }
+
+        return [
+            'available' => array_values(array_unique(Arr::flatten($resultAvailable))),
+            'not_available' => array_values(array_unique(Arr::flatten($resultNotAvailable))),
+        ];
+    }
+
+    public function getAvailableTimes($branch, $service_type, $date)
+    {
+        $reservationInformationMonth = ReservationServiceApi::reservationInformationFiltered($branch, $service_type, $date);
+
+        if (
+            !array_key_exists($branch, $reservationInformationMonth) ||
+            !array_key_exists($service_type, $reservationInformationMonth[$branch])
+        ) {
+            abort(404);
+        }
+
+        $times = $reservationInformationMonth[$branch][$service_type];
+
+        $times['available'] = [];
+        foreach ($times['reservation_times'] as $reservationTime) {
+            $available = true;
+
+            foreach ($times['not_available_datetimes'] as $notAvailableTime) {
+                if (str_contains($notAvailableTime, $reservationTime)) {
+                    $available = false;
+                    break;
+                }
+            }
+
+            if ($available) {
+                array_push($times['available'], $reservationTime);
+            }
+        }
+
+        return $times;
+    }
+
+    public function storeReservation($plate_number, $car_type, $user_type, $IDNumber, $company_id, $branch, $service, $date, $time, $phone)
+    {
+        return ReservationServiceApi::reserve($plate_number, $car_type, $user_type, $IDNumber, $company_id, $branch, $service, $date, $time, $phone);
+    }
+
+    /** Getting resources from request */
+    public function getServicesFromRequest(ServicesRequest $r)
+    {
+        return $this->getServices($r->branch);
+    }
+
+    public function getAvailableDatetimesFromRequest(DatesRequest $r)
+    {
+        return $this->getAvailableDatetimes($r->branch, $r->service_type, $r->year, $r->month);
+    }
+
+    public function getAvailableDatetimesForMultiMonthFromRequest(DatesNMonthRequest $r)
+    {
+        return $this->getAvailableDatetimesForMultiMonth($r->branch, $r->service_type, $r->year, $r->month, $r->n);
+    }
+
+    public function getAvailableTimesFromRequest(TimesRequest $r)
+    {
+        return $this->getAvailableTimes($r->branch, $r->service_type, $r->date);
+    }
+
+    public function storeReservationFromRequest(ReserveRequest $r)
+    {
+        return $this->storeReservation($r->plate_number, $r->car_type, $r->user_type, $r->IDNumber, $r->company_id, $r->branch, $r->service, $r->date, $r->time, $r->phone);
+    }
 }
