2ece5b4267fdf8f7be1380b08135491f7ac400be
CSenshi <saba.pochkhua@gmail.com>
2021-06-10 18:49:16 +0400

    change routes to be defaulted in one file

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Reddot\TegetaReservation\Http\Controllers\ReservationApiController;
-use Reddot\TegetaReservation\Http\Controllers\ReservationViewController;
+use Reddot\TegetaReservation\Facades\ReservationService;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,21 +13,4 @@ use Reddot\TegetaReservation\Http\Controllers\ReservationViewController;
 |
 */
 
-Route::name('reservation.')->prefix('reservation')->group(function () {
-    Route::name('api.')->prefix('api')->group(function () {
-        Route::get('/branches', [ReservationApiController::class, 'branches'])->name('branches');
-        Route::get('/services', [ReservationApiController::class, 'services'])->name('services');
-        Route::get('/dates', [ReservationApiController::class, 'dates'])->name('dates');
-        Route::get('/dates-n-month', [ReservationApiController::class, 'datesNMonth'])->name('dates-n-month');
-        Route::get('/times', [ReservationApiController::class, 'times'])->name('times');
-        Route::post('/reserve', [ReservationApiController::class, 'reserve'])->name('reserve');
-    });
-    Route::name('view.')->prefix('view')->group(function () {
-        Route::get('/branches', [ReservationViewController::class, 'branches'])->name('branches');
-        Route::get('/services', [ReservationViewController::class, 'services'])->name('services');
-        Route::get('/dates', [ReservationViewController::class, 'dates'])->name('dates');
-        Route::get('/dates-n-month', [ReservationViewController::class, 'datesNMonth'])->name('dates-n-month');
-        Route::get('/times', [ReservationViewController::class, 'times'])->name('times');
-        Route::post('/reserve', [ReservationViewController::class, 'reserve'])->name('reserve');
-    });
-});
+ReservationService::routes();
